b2149a825bf99ecf5e8b2e9b34da78e65d82a6d6
SAVERIO MARIA PIERSIGILLI <saveriom.piersigilli@studenti.unicam.it>
2025-01-02 16:51:17 +0100

    init commit

---
+++ src/test/java/com/ids/ProgettoIDS/ProgettoIdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ids.ProgettoIDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoIdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
