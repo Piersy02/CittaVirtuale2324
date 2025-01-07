36e6281097a51dd32d34cb8febb85629da84c3e8
giulia123-l <giulia.lupi@studenti.unicam.it>
2025-01-07 14:08:27 +0100

    Create Comune.java

---
+++ src/main/java/com/ids/ProgettoIDS/Model/Comune.java
@@ -0,0 +1,31 @@
+package com.ids.ProgettoIDS.Model;
+
+import java.util.Date;
+
+public class Comune {
+  private Integer ID;
+  private String nome;
+  private String descrizione;
+  private Posizione posizione;
+  private double area;
+  private Date dataFondazione;
+
+public comune (String nome, String descrizione, Posizione posizione, double area, Date dataFondazione) {
+  this.nome=nome;
+  this.descrizione=descrizione;
+  this.posizione=posizione;
+  this.area=area;
+  this.dataFondazione=dataFondazione;
+}
+  public Comune(){}
+
+public Integer getID(){
+  return ID;
+}
+
+public void setID (Integer ID){
+  this.ID=ID;
+}
+
+}
+
