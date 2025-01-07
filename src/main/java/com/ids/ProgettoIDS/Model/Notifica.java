7c66094f527b22f2a8e5a25baff2b2877b0bd09f
giulia123-l <giulia.lupi@studenti.unicam.it>
2025-01-07 14:35:41 +0100

    chiamata http

---
+++ src/main/java/com/ids/ProgettoIDS/Model/Notifica.java
@@ -0,0 +1,24 @@
+package com.ids.ProgettoIDS.Model;
+
+import java.time.LocalDateTime;
+
+public class Notifica{
+  private Integer Id;
+  private String titolo;
+  private String descrizione;
+  private LocalDateTime timeStamp;
+
+public Notifica(){
+
+}
+
+public Notifica (String titolo, String dascrizione){
+  if (titolo.isBlank() || descrizione.isBlank()){
+    trow new IllegalArgumentException ("titolo o descrizione vuoti");
+  }
+
+  this.titolo= titolo;
+  this.descrizione=descrizione;
+  this.timeStamp = LocalDateTime.now();
+}
+}
