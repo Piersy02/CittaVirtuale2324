925609c9bc6de8218a0761f2937c4c6cbbe0a6cf
giulia123-l <giulia.lupi@studenti.unicam.it>
2025-02-24 08:06:15 +0100

    Update ComuneController.java

--- src/main/java/com/ids/ProgettoIDS/Controller/ComuneController.java
+++ src/main/java/com/ids/ProgettoIDS/Controller/ComuneController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/coumune")
+@RequestMapping("/comune")
 public class ComuneController implements  IController<ComuneDTO, Integer>{
     private final ComuneService comuneService;
 
