8988b8731e3bb923c6e27ccd90c4bccc56f46ad3
giulia123-l <giulia.lupi@studenti.unicam.it>
2025-02-24 08:29:57 +0100

    POI

--- src/main/java/com/ids/ProgettoIDS/Services/ComuneService.java
+++ src/main/java/com/ids/ProgettoIDS/Services/ComuneService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+
 
 @Service
 public class ComuneService implements IComuneService {
