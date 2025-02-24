b0d5f79b58584a8aa831d255ae442f1ca6877862
giulia123-l <giulia.lupi@studenti.unicam.it>
2025-02-24 08:27:46 +0100

    Update Contenuto.java

--- src/main/java/com/ids/ProgettoIDS/Model/Contenuto.java
+++ src/main/java/com/ids/ProgettoIDS/Model/Contenuto.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 
 import java.util.Date;
 @Entity
-@Table(name = "ontenuto")
+@Table(name = "Contenuto")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 
 public abstract class Contenuto {
